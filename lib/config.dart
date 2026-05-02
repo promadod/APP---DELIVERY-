e8aeca5cd6a3a707eecd1221d86e24e494e59806
Michael Alves de Menezes Andre <promadod.trader@gmail.com>
2026-05-02 15:10:45 -0300

    fix: recompila web do cliente 5 com isRede false

--- lib/config.dart
+++ lib/config.dart
@@ -1,12 +1,13 @@
 class Config {
   // 1. O  SERVIDOR
   static const String ipServidor = "127.0.0.1:8000"; //"192.168.11.144:8000";
-  static const String baseUrl = "https://preapdev.pythonanywhere.com"; //"http://127.0.0.1:8000";
-      //"https://preapdev.pythonanywhere.com"; 
+  static const String baseUrl =
+      "https://preapdev.pythonanywhere.com"; //"http://127.0.0.1:8000";
+  //"https://preapdev.pythonanywhere.com";
 
   // Se for gerar o APK do The King, mude para true.
   // Se for gerar o APK da Magno Distribuidora, mude para false.
-  static const bool isRede = false; 
+  static const bool isRede = false;
 
   // Se isRede for false, o App já entra direto nesta loja sem perguntar nada:
   static int lojaId = 5;
