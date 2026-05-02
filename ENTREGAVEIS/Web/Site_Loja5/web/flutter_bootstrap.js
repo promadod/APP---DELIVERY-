2c9af06a15065a7bc2b3ba9583751cd8267b93c6
Michael Alves de Menezes Andre <promadod.trader@gmail.com>
2026-05-02 15:33:53 -0300

    fix: move arquivos web novos para a pasta loja 5

--- ENTREGAVEIS/Web/Site_Loja5/web/flutter_bootstrap.js
+++ ENTREGAVEIS/Web/Site_Loja5/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "931845308"
+    serviceWorkerVersion: "3253468963"
   }
 });
